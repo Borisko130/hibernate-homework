fa03b05c6a96ebab934731e38463817af11c93e8
Borisko130 <krimenyukb@gmail.com>
2020-09-30 16:35:46 +0300

    Homework 35 completed

--- src/main/java/com.dev.cinema/exceptions/HibernateConnectionException.java
+++ src/main/java/com.dev.cinema/exceptions/HibernateConnectionException.java
@@ -1,6 +1,6 @@
 package com.dev.cinema.exceptions;
 
-public class HibernateConnectionException extends RuntimeException{
+public class HibernateConnectionException extends RuntimeException {
     public HibernateConnectionException(String message, Throwable throwable) {
         super(message, throwable);
     }
