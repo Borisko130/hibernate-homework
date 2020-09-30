ffad61ff70fdb44756a545c246af1a5d3f951b8d
Borisko130 <krimenyukb@gmail.com>
2020-09-30 15:33:35 +0300

    Added HibernateUtil class

--- src/main/java/com.dev.cinema/model/Movie.java
+++ src/main/java/com.dev.cinema/model/Movie.java
@@ -1,17 +1,19 @@
 package com.dev.cinema.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
 
     public Movie() {
-    }
-
-    public Movie(Long id, String title, String description) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
     }
 
     public Long getId() {
