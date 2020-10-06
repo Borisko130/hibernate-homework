d9713519d16d8a58167e4da94d54396ac2aa015e
Borisko130 <krimenyukb@gmail.com>
2020-10-06 18:57:31 +0300

    Commit to restart Travis build

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -44,6 +44,7 @@ public class Main {
                 2020, Month.OCTOBER, 6, 22, 20));
         movieSessionService.add(eveningSession);
 
+
         List<MovieSession> sessionList
                 = movieSessionService.findAvailableSessions(movie.getId(), LocalDate.now());
         System.out.println(sessionList);
