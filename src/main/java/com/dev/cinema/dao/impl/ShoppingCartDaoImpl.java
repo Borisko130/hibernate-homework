4463fd625a413d6b13cbe14a552f175af30e4a06
Borisko130 <krimenyukb@gmail.com>
2020-10-08 18:08:26 +0300

    Added DAO and DAO implementations for Ticket and ShoppingCart

---
+++ src/main/java/com/dev/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,24 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.ShoppingCartDao;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.ShoppingCart;
+import com.dev.cinema.model.User;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        return null;
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return null;
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+
+    }
+}
