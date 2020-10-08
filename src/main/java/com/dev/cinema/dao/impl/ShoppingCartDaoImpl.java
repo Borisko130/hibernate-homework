653e8da04eb11ac5890bf850f3aad4331cdff045
Borisko130 <krimenyukb@gmail.com>
2020-10-08 18:50:44 +0300

    Implemented TicketDaoImpl

--- src/main/java/com/dev/cinema/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -1,20 +1,51 @@
 package com.dev.cinema.dao.impl;
 
 import com.dev.cinema.dao.ShoppingCartDao;
+import com.dev.cinema.exceptions.DataProcessingException;
 import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.ShoppingCart;
 import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 @Dao
 public class ShoppingCartDaoImpl implements ShoppingCartDao {
     @Override
     public ShoppingCart add(ShoppingCart shoppingCart) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert ShoppingCart entity for User with id "
+                    + shoppingCart.getUser().getId(), e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public ShoppingCart getByUser(User user) {
-        return null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<ShoppingCart> query = session.createQuery("FROM ShoppingCart "
+                    + "WHERE user.id = :userId", ShoppingCart.class);
+            query.setParameter("userId", user.getId());
+            return query.uniqueResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Failed to find ShoppingCart for user with id "
+                    + user.getId(), e);
+        }
     }
 
     @Override
