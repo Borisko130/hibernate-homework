5d6c6c2bad626c5e50a6fd6378fa6febaea85e4f
Borisko130 <krimenyukb@gmail.com>
2020-10-12 14:16:48 +0300

    Created Order model and implemented OrderDao

---
+++ src/main/java/com/dev/cinema/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.dev.cinema.service;
+
+import com.dev.cinema.model.Order;
+import com.dev.cinema.model.ShoppingCart;
+import com.dev.cinema.model.Ticket;
+import com.dev.cinema.model.User;
+import java.util.List;
+
+public interface OrderService {
+    Order completeOrder(List<Ticket> tickets, User user);
+
+    List<Order> getOrderHistory(User user);
+}
+
+
