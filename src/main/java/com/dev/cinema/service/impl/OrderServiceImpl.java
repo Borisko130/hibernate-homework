88dc87d46e144111551e026ca45b45c409773884
Borisko130 <krimenyukb@gmail.com>
2020-10-12 16:31:03 +0300

    Shopping cart is cleared on order completion

--- src/main/java/com/dev/cinema/service/impl/OrderServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/OrderServiceImpl.java
@@ -1,26 +1,32 @@
 package com.dev.cinema.service.impl;
 
 import com.dev.cinema.dao.OrderDao;
+import com.dev.cinema.dao.ShoppingCartDao;
 import com.dev.cinema.lib.Inject;
 import com.dev.cinema.lib.Service;
 import com.dev.cinema.model.Order;
 import com.dev.cinema.model.Ticket;
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.OrderService;
+import com.dev.cinema.service.ShoppingCartService;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Inject
-    OrderDao orderDao;
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(List<Ticket> tickets, User user) {
         Order order = new Order();
-        order.setOrderDate(LocalDateTime.now());
-        order.setTickets(tickets);
         order.setUser(user);
+        order.setTickets(tickets);
+        order.setOrderDate(LocalDateTime.now());
+        shoppingCartService.clear(shoppingCartService.getByUser(user));
         orderDao.add(order);
         return order;
     }
