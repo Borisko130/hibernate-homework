acfc465d9c7f64d917189ee9bba6f5a4d785a884
Borisko130 <krimenyukb@gmail.com>
2020-10-15 17:20:39 +0300

    Started adding logger to CinemaHallDaoImpl

--- src/main/java/com/dev/cinema/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/CinemaHallDaoImpl.java
@@ -6,14 +6,18 @@ import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.util.HibernateUtil;
 import java.util.List;
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 @Dao
 public class CinemaHallDaoImpl implements CinemaHallDao {
+    private static final Logger logger = Logger.getLogger(CinemaHallDaoImpl.class);
+
     @Override
     public CinemaHall add(CinemaHall cinemaHall) {
+        logger.debug("Method add() invoked");
         Transaction transaction = null;
         Session session = null;
         try {
@@ -21,6 +25,7 @@ public class CinemaHallDaoImpl implements CinemaHallDao {
             transaction = session.beginTransaction();
             session.save(cinemaHall);
             transaction.commit();
+            logger.debug("Method add() invoked");
             return cinemaHall;
         } catch (Exception e) {
             if (transaction != null) {
