84458935149de44f346248f4cdfc03ee080db214
Borisko130 <krimenyukb@gmail.com>
2020-10-21 15:40:58 +0300

    Removed value in GetMapping of sayHello method

--- src/main/java/com/dev/cinema/controller/HelloController.java
+++ src/main/java/com/dev/cinema/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping
     public String sayHello() {
         return "Hello";
     }
